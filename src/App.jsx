b7453d548000b4dab3c8bea1fe6e1752c71e967a
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2023-09-07 18:19:22 +0100

    react app with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-    <div className="bg-red-500">
+    <div className="bg-yellow-500">
         <h1 className="text-3xl font-bold underline">
           Hello world!
         </h1> 
