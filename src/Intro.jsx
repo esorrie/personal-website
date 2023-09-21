a82e2b114f94cca3895d865cd4296bd7bea4eaed
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2023-09-21 18:18:13 +0100

    created an intro, with tailwind and raw css

---
+++ src/Intro.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Intro.css'
+
+const Intro = () => {
+  return (
+    <div className="ml-136 mt-280">
+      <div className="title">
+        Full Stack Web Developer
+      </div> 
+      <div className="card1 mt-4">
+        Welcome to my corner of the internet!
+      </div>
+      <div className="card2 mt-5">
+        I'm thrilled to introduce myself as a recent Computer Engineering graduate with an interest in the realm of full stack web development. I have a strong foundation in both front-end and back-end technologies. While I enjoy the prowess to craft complete and immersive online experiences from concept to deployment.
+      </div>
+    </div>
+  );
+};
+export default Intro;
