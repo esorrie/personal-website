62a6ff2bc0d551d2737b85eea6ebaa2630a6decd
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2023-09-21 18:54:09 +0100

    fixed indents

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import './NavBar.css'
 
 const Navbar = () => {
@@ -15,7 +15,6 @@ const Navbar = () => {
             </div>
         </nav>
     );
-
 
 }; 
 export default Navbar;
