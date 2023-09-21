40cf4796f91fedd8d7ef2e1cec69bc01103a9023
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2023-09-21 18:33:24 +0100

    created folders for each section

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Intro from './Intro/Intro.jsx'
+import NavBar from './Navbar/NavBar.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <NavBar />
+    <Intro />
+    {/* <About /> */}
+    {/* <Portfolio /> */}
+    {/* <Contact /> */}
   </React.StrictMode>,
 )
