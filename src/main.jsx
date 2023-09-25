b9c212cdf4e677156362089f30edbf14e718d97b
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2023-09-25 22:10:04 +0100

    displaying portfolio on page

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import NavBar from './NavBar.jsx'
 import Intro from './Intro.jsx'
 import About from './About.jsx'
+import Portfolio from './Portfolio.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -10,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <NavBar />
     <Intro />
     <About />
-    {/* <Portfolio /> */}
+    <Portfolio />
     {/* <Contact /> */}
   </React.StrictMode>,
 )
