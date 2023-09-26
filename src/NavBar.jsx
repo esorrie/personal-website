3763cefbf4e4e5048918ecf104f10608043864e2
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2023-09-26 13:55:59 +0100

    added TODO and removed unused style

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -7,10 +7,11 @@ const Navbar = () => {
             <div className="menu">
                 <div className="navtitle text-custom_blue text-2xl font-semibold">Edward Sorrie</div>
             </div>
+            {/* TODO = fix scroll on navbar links */}
             <div className="menu navitem text-white">
-                <a href="#about_section" className="navitem font-base font-medium cursor-default hover:text-custom_blue scroll"> About </a>
-                <a href="#portfolio_section" className="navitem font-base font-medium cursor-default hover:text-custom_blue"> Portfolio </a>
-                <a href="#contact_section" className="font-base font-medium cursor-default hover:text-custom_blue"> Contact Me </a>
+                <a href="#about_section" className="navitem font-base font-medium cursor-default hover:text-custom_blue scroll-smooth"> About </a>
+                <a href="#portfolio_section" className="navitem font-base font-medium cursor-default hover:text-custom_blue scroll-smooth"> Portfolio </a>
+                <a href="#contact_section" className="font-base font-medium cursor-default hover:text-custom_blue scroll-smooth"> Contact Me </a>
             </div>
         </nav>
     );
