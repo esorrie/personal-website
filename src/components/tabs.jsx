50c2c28abccca1fadb101e989278a83e008acd04
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2023-09-26 12:42:42 +0100

    added info to education tab

--- src/components/tabs.jsx
+++ src/components/tabs.jsx
@@ -24,7 +24,7 @@ function Tabs()  {
                     <div className={toggleState === 1 ? "tabs cursor-default active-tabs" : "tabs cursor-default" } onClick={() => toggleTab(1)} > skills </div>
                     <div className={toggleState === 2 ? "tabs cursor-default active-tabs" : "tabs cursor-default" } onClick={() => toggleTab(2)} > education </div>
                     <div className={toggleState === 3 ? "tabs cursor-default active-tabs" : "tabs cursor-default" } onClick={() => toggleTab(3)} > experience </div>
-                    <a className="tabs cursor-pointer" > cv </a>
+                    <a href="https://1drv.ms/w/s!AsrSQj_iC3W5g1blTLW0n05iwYRl?e=k9ltRL" className="tabs cursor-pointer" > cv </a>
                 </div>
 
                 <div className="content-tabs w-552">
@@ -41,7 +41,12 @@ function Tabs()  {
                         </ul>
                     </div>
                     <div className={toggleState === 2 ? "content active-content" : "content" } >
-                    Third year student at Newcastle University studying a BEng Hons in Electronic and Computer Engineering
+                        Studied at King Edwards VI high school for mmy GCSE's and A-levels; <br/>
+                        GCSE's; <br/>
+                        I passed all 10 subjects achieving three 7's, two 6's, two 5's, three 4's, along with an optional 11th subject of Further Mathematics achieving a C. <br/>
+                        A-levels; <br/> 
+                        I achieved a B in Business Studies and C in both Mathematics and Physics. <br/> <br/>
+                        I am currently a third year student at Newcastle University studying a BEng Hons in Electronic and Computer Engineering achieving a 2:1 in my second year.
                     </div>
                     <div className={toggleState === 3 ? "content active-content" : "content" } >
                         6 week internship at Woods&Walker
