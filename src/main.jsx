c3fa2da4ac7956c6a453e04ab2addf90f5749fe9
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2023-09-26 11:56:43 +0100

    added  a link to my linkedin profile, need to make a link to my email

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import NavBar from './NavBar.jsx'
 import Intro from './Intro.jsx'
 import About from './About.jsx'
 import Portfolio from './Portfolio.jsx'
+import Contact from './Contact.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -12,6 +13,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Intro />
     <About />
     <Portfolio />
-    {/* <Contact /> */}
+    <Contact />
   </React.StrictMode>,
 )
