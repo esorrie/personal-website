f8d6478debb44a1f280c792e0bdc760bb20f1f81
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2023-10-09 13:11:55 +0100

    changed layout of intro section

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -3,14 +3,14 @@ import './Intro.css'
 
 const Intro = () => {
     return (
-        <div className="ml-136 mt-200">
-            <div className="title">
+        <div className="text-center mt-200 ">
+            <div className="title ">
               Full Stack Web Developer
             </div> 
             <div className="card1 mt-4">
               Welcome to my corner of the internet!
             </div>
-            <div className="card2 mt-5">
+            <div className="min-w-min card2 mx-136 mt-5">
               I'm thrilled to introduce myself as a current third year student from Newcastle with an interest in the realm of full stack web development. I have a strong foundation in both front-end and back-end technologies. With a passion to craft complete and immersive online experiences from concept to deployment.
             </div>
         </div>
