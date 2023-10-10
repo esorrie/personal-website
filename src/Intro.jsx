d6b65edbad4297d111f3140493f34dc8e2ec1247
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2023-10-10 13:11:48 +0100

    fixed intro section layout for screen size

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -3,6 +3,7 @@ import './Intro.css'
 
 const Intro = () => {
     return (
+      <>
         <div className="text-center mt-200 ">
             <div className="title ">
               Full Stack Web Developer
@@ -10,10 +11,13 @@ const Intro = () => {
             <div className="card1 mt-4">
               Welcome to my corner of the internet!
             </div>
-            <div className="mx-272 card2 mt-5">
-              I'm thrilled to introduce myself as a current third year student from Newcastle with an interest in the realm of full stack web development. I have a strong foundation in both front-end and back-end technologies. With a passion to craft complete and immersive online experiences from concept to deployment.
-            </div>
         </div>
+        <div className="text-center">
+          <div className="px-18.9% card2 mt-5">
+            I'm thrilled to introduce myself as a current third year student from Newcastle with an interest in the realm of full stack web development. I have a strong foundation in both front-end and back-end technologies. With a passion to craft complete and immersive online experiences from concept to deployment.
+          </div>
+        </div>
+      </>
     );
 };
 export default Intro;
