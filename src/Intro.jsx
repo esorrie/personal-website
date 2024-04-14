1fe12ea9c8c4d01dc98cd3fc4e9c408e92dda1d0
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2024-04-14 14:52:13 +0100

    corrected grammar

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -14,7 +14,7 @@ const Intro = () => {
         </div>
         <div className="text-center">
           <div className="px-18.9% card2 mt-5">
-            I'm thrilled to introduce myself as a current third year student from Newcastle with an interest in the realm of full stack web development. I have a strong foundation in both front-end and back-end technologies. With a passion to craft complete and immersive online experiences from concept to deployment.
+            I'm thrilled to introduce myself as a current third year student from Newcastle with an interest in the realm of full stack development. I have a strong foundation in both front-end and back-end technologies. With a passion to craft and complete immersive online experiences from concept to deployment.
           </div>
         </div>
       </>
